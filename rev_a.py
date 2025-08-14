9a89163970fc074f4414e2ce34bf340999de68bf
Miles Punch <71351127+miles-p@users.noreply.github.com>
2025-08-14 21:42:28 +1000

    first result. positive AI response.

--- rev_a.py
+++ rev_a.py
@@ -1,3 +1,15 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-client = OpenAI(api_key=)
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response = client.responses.create(
+    model="gpt-5-nano-2025-08-07",
+    input="How many floors is the Empire State Building?",
+    instructions="Respond only with what is necessary; A good answer is a short answer."
+)
+
+print(response.output_text)
